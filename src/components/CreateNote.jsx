b38db811fc236269a2a5d9d2fa9ff8262abd94ed
Onur Han <onurhan@Onur-MacBook-Air.local>
2025-03-23 02:58:54 +0300

    note header fixed

--- src/components/CreateNote.jsx
+++ src/components/CreateNote.jsx
@@ -32,7 +32,10 @@ export default function CreateNote() {
 
   return (
     <div className="flex flex-col items-center justify-center">
-      <div className="mb-2 border-b w-full py-1 border-[#6c757d] flex items-center gap-5">
+            <div className="w-full h-[50px] max-w-md mb-3 rounded p-1">
+        <input className="w-full h-full" type="text" placeholder="Note Header" />
+      </div>
+      <div className="mb-5 border-b w-full py-1 border-[#6c757d] flex items-center gap-5">
         <div className="flex items-center gap-2 pr-1 border-r border-r-[#6c757d]">
           <i className="fa-solid fa-tag "></i>
           {
@@ -42,9 +45,6 @@ export default function CreateNote() {
         <div>
           {selectedTags.map(t => t).join(", ")}
         </div>
-      </div>
-      <div className="w-full h-[50px] max-w-md mb-3 rounded p-1">
-        <input className="w-full h-full" type="text" placeholder="Note Header" />
       </div>
       <div className="w-screen h-screen max-w-md">
         <textarea className="w-full p-1 h-[50%] rounded" placeholder="Take Note" ></textarea>
