073479616ef004cad6491c3948cc9e60348ee22d
Onur Han <onurhan@Onur-MacBook-Air.local>
2025-03-23 11:50:26 +0300

    align apply

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -2,8 +2,8 @@ import { FaArrowLeft, FaCalculator, FaDownload, FaTrash } from "react-icons/fa";
 
 export default function Sidebar() {
     return(
-        <div className="flex justify-center flex-col text-[#8d8d91]">
-            <div className="flex justify-between w-screen pr-[20px] pl-[20px] mb-[10px]">
+        <div className="flex justify-center flex-col  text-[#8d8d91]">
+            <div className="flex justify-between w-screen p-[20px]">
             <div className="flex  items-center gap-[20px]">
             <FaArrowLeft />
             <p>Go Back</p>
@@ -15,7 +15,7 @@ export default function Sidebar() {
                 <button className="text-cyan-600">Save Note</button>
             </div>
             </div>
-            <div className="border border-b-1 border-[#8d8d91] mb-[20px] mr-[20px] ml-[20px]">
+            <div className="border-r outline-none border-b-1 border-[#8d8d91] mb-[20px] mr-[20px] ml-[20px]">
             
             </div>
         </div>
