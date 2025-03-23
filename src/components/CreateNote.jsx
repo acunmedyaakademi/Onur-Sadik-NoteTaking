2348aaf4f408e75dc955827f821b610e4f465550
Onur Han <onurhan@Onur-MacBook-Air.local>
2025-03-23 11:28:42 +0300

    createnote reworked

--- src/components/CreateNote.jsx
+++ src/components/CreateNote.jsx
@@ -31,11 +31,11 @@ export default function CreateNote() {
 
 
   return (
-    <div className="flex flex-col items-center justify-center">
-            <div className="w-full h-[50px] max-w-md mb-3 rounded p-1">
+    <div className="flex flex-col items-center justify-center pr-[20px] pl-[20px]">
+            <div className="w-full pr-[20px] pl-[20px] h-[50px] mb-3 rounded p-1">
         <input className="w-full h-full" type="text" placeholder="Note Header" />
       </div>
-      <div className="mb-5 border-b w-full py-1 border-[#6c757d] flex items-center gap-5">
+      <div className="mb-5 pr-[20px] pl-[20px] border-b w-full py-1 border-[#6c757d] flex items-center gap-5">
         <div className="flex items-center gap-2 pr-1 border-r border-r-[#6c757d]">
           <i className="fa-solid fa-tag "></i>
           {
@@ -46,7 +46,7 @@ export default function CreateNote() {
           {selectedTags.map(t => t).join(", ")}
         </div>
       </div>
-      <div className="w-screen h-screen max-w-md">
+      <div className="w-screen h-screen pr-[20px] pl-[20px]">
         <textarea className="w-full p-1 h-[50%] rounded" placeholder="Take Note" ></textarea>
       </div>
     </div>
