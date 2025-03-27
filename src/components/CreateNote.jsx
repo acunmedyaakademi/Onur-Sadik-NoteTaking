e1ea74e2853f8a7d61955f466fc280f53736e02b
onur han hacimustafaoglu <onurhanhacimustafaoglu@gmail.com>
2025-03-27 19:26:13 +0300

    r-bar ok

--- src/components/CreateNote.jsx
+++ src/components/CreateNote.jsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import tags from "../configs/tagNames";
 import DefineTags from "./DefineTags";
+import { FaDownload, FaSearch, FaTrash } from "react-icons/fa";
+import { FaDeleteLeft, FaGear, FaUpDown } from "react-icons/fa6";
 
 
 export default function CreateNote() {
@@ -45,17 +47,37 @@ export default function CreateNote() {
         </div>
       </div>
       <div className="xl:col-span-9">
-        <div className="xl:p-[40px] xl:pb-[20px] border-b xl:border-l">
+        <div className="xl:p-[40px] xl:pb-[20px] border-b xl:border-l flex justify-between">
           <div>
           <h1>All Notes</h1>
           </div>
-          <div>
-            <input type="text" />
+          <div className="flex">
+          <div class="">
+                <FaSearch />
+                <input type="text" id="searchInput" placeholder="Search by title, content or tags..."/>
+            </div>
+            <div>
+              <FaGear />
+            </div>
           </div>
+
         </div>
         <div className="xl:grid xl:grid-cols-12 xl:h-screen xl:border-l">
-        <div className="xl:border-r xl:h-screen xl:col-span-3 hidden xl:block xl:p-5">
-        <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit tempore cum nostrum accusantium totam? Facere, et odit sapiente odio placeat porro magnam, ex non eaque neque culpa libero repellat provident!</h1>
+        <div className="xl:border-r xl:h-screen xl:col-span-3 hidden xl:block xl:py-5 xl:px-5">
+        <div className="rounded-lg bg-[#325CFE] text-[#FFFFFF] py-[15px] w-full flex items-center justify-center mb-5">
+          <button>+ Create New Note</button>
+        </div>
+        <div className=" hover:bg-[#F4F5F8] rounded-lg p-2">
+        <div>
+          <h1>React Performance Optimization</h1>
+        </div>
+        <button>Dev</button>
+        <button>React</button>
+
+        <div>
+          <p> 29 Oct 2024</p>
+        </div>
+        </div>
         </div>
         <div className="xl:col-span-6 xl:border-r flex flex-col items-center justify-center xl:p-5">
         <div className="w-full pr-[20px] pl-[20px] h-[50px] mb-3 rounded p-1">
@@ -84,7 +106,16 @@ export default function CreateNote() {
         </div>
       </div>
         <div className="xl:col-span-3 hidden xl:block xl:p-5">
-            <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet quod quasi doloremque, mollitia necessitatibus temporibus provident pariatur perferendis animi facilis? Nemo, odio distinctio magnam cum modi impedit quam. Perspiciatis, dolorem.</h1>
+            <div className="flex flex-col gap-5">
+              <div className="border-[#F4F5F8] border w-full rounded-lg flex items-center justify-start gap-5 pl-5 py-4 bg-[#FFFFFF] shadow-lg text-[#36363E] hover:bg-[#F4F5F8] cursor-pointer">
+              <FaDownload />
+              <button className="text-base/8"> Archive Note</button>
+              </div>
+              <div className="border-[#F4F5F8] border w-full rounded-lg flex items-center justify-start gap-5 pl-5 py-4 bg-[#FFFFFF] shadow-lg text-[#36363E] hover:bg-[#F4F5F8] cursor-pointer">
+              <FaTrash />
+              <button className="text-base/8"> Delete Note</button>
+              </div>
+            </div>
         </div>
         </div>
 
