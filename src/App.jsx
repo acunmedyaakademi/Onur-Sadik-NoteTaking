ec1a363c5cd733d3ed67c641a75b40f5131cd8ac
onur han hacimustafaoglu <onurhanhacimustafaoglu@gmail.com>
2025-04-07 22:32:13 +0300

    app fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,21 @@
 import { useState } from 'react'
 import Homepage from './components/HomePage'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import AllNotes from './components/AllNotes';
+import ArchivedNotes from './components/ArchivedNotes';
 
 function App() {
   const [page, setPage] = useState("");
 
 
   return (
-    <>
-      <Homepage />
-    </>
+    <Router>
+    <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/allnotes" element={<AllNotes />} />
+        <Route path="/archivednotes" element={<ArchivedNotes />} />
+    </Routes>
+</Router>
   )
 }
 
